31fefdb87518188453e4dbfecbfa16cf0a35711e
MartyChristopher <519483040@qq.com>
2023-09-14 15:07:00 +0800

    no message

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Apple on 2023/9/14.
+//
+
+import UIKit
+import ZLVideoPlayer
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
